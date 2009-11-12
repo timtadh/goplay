2efe1d2acf91b4aaf0932958493e7cef9903d64a
Tim Henderson <tim.tadh@gmail.com>
2009-11-12 14:55:16 -0500

    initial commit
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ fib/fib.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+
+func fib(i int) (r int)
+{
+    if (i == 0) { r = 0; }
+    else if (i == 1) { r = 1; }
+    else { r = fib(i-2) + fib(i-1)}
+    fmt.Println(r);
+    return
+}
+
+func main()
+{
+    fmt.Println(fib(5));
+}
