faecd24715801f0a180cb3c9ec95509f02930920
Tim Henderson <tim.tadh@gmail.com>
2009-11-14 20:59:00 -0500

    fib updated
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- fib/fib.go
+++ fib/fib.go
@@ -1,15 +1,19 @@
 package main
 
-import "fmt"
-
+import ("fmt"; "syscall");
 
 func fib(i int) (r int)
 {
     if (i == 0) { r = 0; }
     else if (i == 1) { r = 1; }
     else { r = fib(i-2) + fib(i-1)}
-    fmt.Println(r);
     return
+}
+
+func video()
+{
+    fmt.Println(syscall.Getpid());
+    //fmt.Println(VideoShutdown());
 }
 
 func main()
